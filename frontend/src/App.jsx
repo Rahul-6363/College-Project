71c81e54f5a1eea79124d107cb91537af61e3f5b
Rahul P <rrahulrahul220@gmail.com>
2026-04-22 13:23:53 +0530

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider, useAuth } from './context/AuthContext';
 import Login from './pages/Login';
-import Signup from './pages/Signup';
+import Signup from './pages/SignUp';
 import DataManagerDashboard from './pages/DataManagerDashboard';
 import PrincipalDashboard from './pages/PrincipalDashboard';
 
